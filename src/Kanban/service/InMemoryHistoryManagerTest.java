a10b088d2ac7783e5199163afad9a887f0763f6b
DobryakovRoman <griffinsc@ya.ru>
2022-09-27 00:00:13 +0300

    0.0.28 начало запуска тестов jUnit

--- src/Kanban/service/InMemoryHistoryManagerTest.java
+++ src/Kanban/service/InMemoryHistoryManagerTest.java
@@ -30,7 +30,21 @@ class InMemoryHistoryManagerTest {
         taskManager.addEpic(epic);
         taskManager.getEpicById(1);
         assertEquals(epic, taskManager.getHistory().get(0));
-
+        LocalDateTime now = LocalDateTime.now();
+        Task task = new Task("Задача 1", "Создать задачу 1", now, 10);
+        taskManager.addTask(task);
+        taskManager.getTaskById(2);
+        assertEquals(task, taskManager.getHistory().get(1));
+        Subtask subtask1 = new Subtask("st1", "st1", now, 10);
+        subtask1.setEpicid(epic.getid());
+        taskManager.addSubtask(subtask1);
+        taskManager.getSubtaskByid(3);
+        assertEquals(subtask1, taskManager.getHistory().get(2));
+        taskManager.removeEpic(1);
+        assertEquals(1, taskManager.getHistory().size());
+        assertEquals(task, taskManager.getHistory().get(0));
+        RuntimeException ex = new taskManager.getHistory().get(1)
+        assertEquals("Hello", ex.getMessage());
     }
 
     @Test
