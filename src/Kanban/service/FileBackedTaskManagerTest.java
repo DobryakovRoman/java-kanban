e755624619715c90fc3faa2254e385c6e2f22f10
DobryakovRoman <griffinsc@ya.ru>
2022-10-30 00:39:37 +0300

    0.0.32 feat: добавлены тесты

--- src/Kanban/service/FileBackedTaskManagerTest.java
+++ src/Kanban/service/FileBackedTaskManagerTest.java
@@ -1,12 +1,14 @@
 package Kanban.service;
 
 import Kanban.task.Epic;
+import Kanban.task.Subtask;
 import Kanban.task.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -89,8 +91,8 @@ class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager>{
 
     @Test
     void updateEpic() {
-        File fileNameGetEpicById = new File("data_updateEpic.csv");
-        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameGetEpicById);
+        File fileNameUpdateEpic = new File("data_updateEpic.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameUpdateEpic);
         taskManager.updateEpic(new Epic("ep1", "ep1"));
         assertNull(taskManager.getEpicById(1));
         taskManager.addEpic(new Epic("ep1", "ep1"));
@@ -102,38 +104,113 @@ class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager>{
 
     @Test
     void removeEpic() {
-
+        File fileNameRemoveEpic = new File("data_removeEpic.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameRemoveEpic);
+        Epic epic = new Epic("ep2", "ep2");
+        epic.setid(1);
+        taskManager.addEpic(epic);
+        taskManager.removeEpic(2);
+        assertEquals(1, taskManager.getEpics().size());
+        epic = new Epic("ep2", "ep2");
+        epic.setid(2);
+        taskManager.addEpic(epic);
+        assertEquals(2, taskManager.getEpics().size());
+        taskManager.removeEpic(2);
+        assertEquals(1, taskManager.getEpics().size());
+        assertEquals("ep2", taskManager.getEpicById(1).getTitle());
+        assertEquals(taskManager.getEpics(), taskManagerExpected.getEpics());
     }
 
     @Test
     void addSubtask() {
+        File fileNameAddSubtask = new File("data_addSubtask.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameAddSubtask);
+        assertEquals(0, taskManager.getSubtasks().size());
+        initSubtask();
+        assertEquals(1, taskManager.getSubtasks().size());
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 
     @Test
     void getSubtaskByid() {
+        File fileNameGetSubtaskById = new File("data_getSubtaskById.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameGetSubtaskById);
+        assertNull(taskManager.getSubtaskByid(2));
+        initSubtask();
+        assertEquals("st1", taskManager.getSubtaskByid(2).getTitle());
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 
     @Test
     void updateSubtask() {
+        File fileNameUpdateSubtask = new File("data_updateSubtask.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameUpdateSubtask);
+        assertNull(taskManager.getSubtaskByid(2));
+        initSubtask();
+        Subtask subtask = taskManager.getSubtaskByid(2);
+        subtask.setDescription("st2");
+        taskManager.updateSubtask(subtask);
+        assertEquals("st1", taskManager.getSubtaskByid(2).getTitle());
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 
     @Test
     void removeSubtask() {
+        File fileNameRemoveSubtask = new File("data_removeSubtask.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameRemoveSubtask);
+        assertNull(taskManager.getSubtaskByid(2));
+        initSubtask();
+        assertEquals("st1", taskManager.getSubtaskByid(2).getTitle());
+        taskManager.removeSubtask(2);
+        assertNull(taskManager.getSubtaskByid(2));
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 
     @Test
     void clear() {
+        File fileNameClear = new File("data_clear.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameClear);
+        assertEquals(0, taskManager.getEpics().size());
+        assertEquals(0, taskManager.getTasks().size());
+        assertEquals(0, taskManager.getSubtasks().size());
+        initAll();
+        assertEquals(1, taskManager.getEpics().size());
+        assertEquals(3, taskManager.getTasks().size());
+        assertEquals(1, taskManager.getSubtasks().size());
+        taskManager.clear();
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 
     @Test
     void clearTasks() {
+        File fileNameClearTasks = new File("data_clearTasks.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameClearTasks);
+        assertEquals(0, taskManager.getTasks().size());
+        initTasks();
+        assertEquals(3, taskManager.getTasks().size());
+        taskManager.clearTasks();
+        assertEquals(taskManager.getTasks(), taskManagerExpected.getTasks());
     }
 
     @Test
     void clearEpics() {
+        File fileNameClearEpics = new File("data_clearEpics.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameClearEpics);
+        assertEquals(0, taskManager.getEpics().size());
+        initAll();
+        assertEquals(1, taskManager.getEpics().size());
+        taskManager.clearEpics();
+        assertEquals(taskManager.getEpics(), taskManagerExpected.getEpics());
     }
 
     @Test
     void clearSubtasks() {
+        File fileNameClearSubtasks = new File("data_clearSubtasks.csv");
+        FileBackedTaskManager taskManagerExpected = new FileBackedTaskManager(fileNameClearSubtasks);
+        assertEquals(0, taskManager.getSubtasks().size());
+        initAll();
+        assertEquals(1, taskManager.getSubtasks().size());
+        taskManager.clearSubtasks();
+        assertEquals(taskManager.getSubtasks(), taskManagerExpected.getSubtasks());
     }
 }
--- src/Kanban/service/TaskManagerTest.java
+++ src/Kanban/service/TaskManagerTest.java
@@ -1,6 +1,8 @@
 package Kanban.service;
 
 import Kanban.service.TaskManager;
+import Kanban.task.Epic;
+import Kanban.task.Subtask;
 import Kanban.task.Task;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +28,24 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         taskManager.getTaskById(3);
     }
 
+    protected void initSubtask() {
+        taskManager.addEpic(new Epic("ep1", "ep1"));
+        Epic epic = taskManager.getEpicById(1);
+        Subtask subtask1 = new Subtask("st1", "st1", now, 10);
+        subtask1.setEpicid(epic.getid());
+        taskManager.addSubtask(subtask1);
+    }
+
+    protected void initAll() {
+        taskManager.addTask(new Task("Задача 1", "Создать задачу 1", now, 10));
+        taskManager.addTask(new Task("Задача 2", "Создать задачу 2", now.plusMinutes(30), 20));
+        taskManager.addTask(new Task("Задача 3", "Создать задачу 3", now.plusMinutes(50), 30));
+        taskManager.addEpic(new Epic("ep1", "ep1"));
+        Epic epic = taskManager.getEpicById(4);
+        Subtask subtask1 = new Subtask("st1", "st1", now, 10);
+        subtask1.setEpicid(epic.getid());
+        taskManager.addSubtask(subtask1);
+    }
 
 
 }
